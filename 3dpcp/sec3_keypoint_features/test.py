e4718fb8b4ac48d9cbac31b8cb2a998f0827757e
OsawaKousei <jwithelis@gmail.com>
2024-04-22 17:19:14 +0900

    update

--- 3dpcp/sec3_keypoint_features/test.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print(sys.prefix)
-print(sys.path)
-import open3d
--- docker/setup.sh
+++ docker/setup.sh
@@ -3,4 +3,5 @@ sudo apt update
 sudo apt upgrade -y
 
 # make workspace
-git clone
+cd ~
+sudo git clone https://github.com/OsawaKousei/3dpcp_ws.git
